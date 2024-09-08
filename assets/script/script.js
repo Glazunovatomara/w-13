0912e8c2e3c99626cfd86a17a9c512a336039111
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-09-09 02:38:24 +0700

    feat:add type for button

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,15 +4,23 @@ const button = document.getElementById('btn');
 const showMessage = () => {
     const result = document.querySelector('.section__date_text');
     const date = document.getElementById('calendar').value;
-    console.log(`date ${date}`)
+    
     const dateTime = Date.parse(date)
-    console.log(`dateTime %{dateTime}`)
 
     const currentDate = Date.now();
 
     const calculateDays = dateTime - currentDate;
     let daysPassed = Math.floor( calculateDays / (1000 * 60 * 60 * 24) );
 console.log( `currentDate ${currentDate}`)
+
+//if (daysPassed[-1] === 1 && daysPassed !== 11 && daysPassed !== 111 && daysPassed !== 211 && daysPassed !== 311) {
+   // day = 'день'
+//} else if (daysPassed[-1] === 2,3,4,) {
+//    day = 'дня'
+//} else {
+  //  day = 'дней' 5 6 7 8 9 10 11 -20
+//}
+
     if(daysPassed > 0){
         result.textContent = `Мероприятие будет через ${daysPassed}`
     } else if(daysPassed = 0) {
