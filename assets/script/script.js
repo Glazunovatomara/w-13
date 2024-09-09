62f1892545625248d768c6d59029bebbc09d9ef1
Glazunova Tomara <glazunovatomara5@gmail.com>
2024-09-10 01:16:45 +0700

    feat:add day

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,7 @@
 const button = document.getElementById('btn');
 
 const showMessage = () => {
-    const result = document.querySelector('.section__date_text');
+    const str = document.querySelector('.section__date_text');
     const date = document.getElementById('calendar').value;
     
     const dateTime = Date.parse(date)
@@ -12,23 +12,29 @@ const showMessage = () => {
     const calculateDays = dateTime - currentDate;
     let daysPassed = Math.floor( calculateDays / (1000 * 60 * 60 * 24) );
 
-//if (daysPassed[-1] === 1 && daysPassed !== 11 && daysPassed !== 111 && daysPassed !== 211 && daysPassed !== 311) {
-   // day = 'день'
-//} else if (daysPassed[-1] === 2,3,4,) {
-//    day = 'дня'
-//} else {
-  //  day = 'дней' 5 6 7 8 9 10 11 -20
-//}
+    let day = 'день';
+    const daysPassedStr = daysPassed.toString();
 
+    if (daysPassedStr.charAt(daysPassedStr.length - 1) === '1' && daysPassedStr !== '11' && daysPassedStr !=='111' && daysPassedStr !=='211' && daysPassedStr !=='311') {
+        day = 'день'
+    } else if (daysPassedStr.charAt(daysPassedStr.length - 1) === '2' && daysPassedStr !== '12' && daysPassedStr !=='112' && daysPassedStr !=='212' && daysPassedStr !=='312') {
+        day = 'дня'
+    } else if(daysPassedStr.charAt(daysPassedStr.length - 1) ==='3' && daysPassedStr !== '13' && daysPassedStr !=='113' && daysPassedStr !=='213' && daysPassedStr !=='313') {
+        day = 'дня'
+    } else if(daysPassedStr.charAt(daysPassedStr.length - 1) ==='4'  && daysPassedStr !== '14' && daysPassedStr !=='114' && daysPassedStr !=='214' && daysPassedStr !=='314') {
+        day = 'дня'
+    } else {
+        day = 'дней' 
+    }
     if(daysPassed > 0){
-        result.textContent = `Мероприятие будет через ${daysPassed}`
+        str.textContent = `Мероприятие будет через ${daysPassed} ${day}`
     } else if(daysPassed === 0) {
-        result.textContent = `Мероприятие сегодня!`
+        str.textContent = `Мероприятие сегодня!`
     } else if(daysPassed < 0){
         daysPassed = Math.abs(daysPassed);
-        result.textContent = `Мероприятие состоялось ${daysPassed} назад`
+        str.textContent = `Мероприятие состоялось ${daysPassed} ${day} назад`
     } else {
-        result.textContent = `Чтобы посчитать колличество дней до события, вам нужно выбрать интересующую вас дату`
+        str.textContent = `Чтобы посчитать колличество дней до события, вам нужно выбрать интересующую вас дату`
     }
 }
 
